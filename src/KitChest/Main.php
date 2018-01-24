d36292c29a3dac5a7721b21397c7987fd30f445f
Blazing Games <iiflamiinblaze123@gmail.com>
2018-01-24 11:01:49 -0500

    Implementing Kit Permissions

--- src/KitChest/Main.php
+++ src/KitChest/Main.php
@@ -10,12 +10,13 @@ use pocketmine\item\Item;
 use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\enchantment\EnchantmentInstance;
 use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\utils\TextFormat;
 
 class Main extends PluginBase implements Listener{
 
     public function onEnable(){
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
-        $this->getServer()->getCommandMap()->register("kitgive", new KitChestGiveCommand($this));
+        $this->getServer()->getCommandMap()->register("kit", new KitChestGiveCommand($this));
     }
 
     public function onInteract(PlayerInteractEvent $event){
@@ -24,31 +25,37 @@ class Main extends PluginBase implements Listener{
             $damage = $event->getItem()->getDamage();
             switch($damage){
                 case 10:
-                    $helmet = Item::get(310, 0, 1)->setCustomName("Test helmet");
-                    $e = Enchantment::getEnchantment((int)0);
-                    $helmet->addEnchantment(new EnchantmentInstance($e, (int)-0));
-                    $e = Enchantment::getEnchantment((int)0);
-                    $helmet->addEnchantment(new EnchantmentInstance($e, (int)-0));
-                    $chestplate = Item::get(311, 0, 1)->setCustomName("Test Chestplate");
-                    $e = Enchantment::getEnchantment((int)0);
-                    $chestplate->addEnchantment(new EnchantmentInstance($e, (int)-0));
-                    $e = Enchantment::getEnchantment((int)0);
-                    $chestplate->addEnchantment(new EnchantmentInstance($e, (int)-0));
-                    $leggings = Item::get(312, 0, 1)->setCustomName("Test Leggings");
-                    $e = Enchantment::getEnchantment((int)0);
-                    $leggings->addEnchantment(new EnchantmentInstance($e, (int)-0));
-                    $e = Enchantment::getEnchantment((int)0);
-                    $leggings->addEnchantment(new EnchantmentInstance($e, (int)-0));
-                    $boots = Item::get(313, 0, 1)->setCustomName("Test Boots");
-                    $e = Enchantment::getEnchantment((int)0);
-                    $boots->addEnchantment(new EnchantmentInstance($e, (int)-0));
-                    $e = Enchantment::getEnchantment((int)0);
-                    $boots->addEnchantment(new EnchantmentInstance($e, (int)-0));
-                    $player->getInventory()->addItem($helmet);
-                    $player->getInventory()->addItem($chestplate);
-                    $player->getInventory()->addItem($leggings);
-                    $player->getInventory()->addItem($boots);
-                    $player->getInventory()->removeItem($item);
+                    if($player->hasPermission("kit.test")){
+                        $item = $player->getInventory()->getItem("54");
+                        $item->setCustomName("Test kit");
+                        $helmet = Item::get(310, 0, 1)->setCustomName("Test helmet");
+                        $e = Enchantment::getEnchantment((int)0);
+                        $helmet->addEnchantment(new EnchantmentInstance($e, (int)-0));
+                        $e = Enchantment::getEnchantment((int)0);
+                        $helmet->addEnchantment(new EnchantmentInstance($e, (int)-0));
+                        $chestplate = Item::get(311, 0, 1)->setCustomName("Test Chestplate");
+                        $e = Enchantment::getEnchantment((int)0);
+                        $chestplate->addEnchantment(new EnchantmentInstance($e, (int)-0));
+                        $e = Enchantment::getEnchantment((int)0);
+                        $chestplate->addEnchantment(new EnchantmentInstance($e, (int)-0));
+                        $leggings = Item::get(312, 0, 1)->setCustomName("Test Leggings");
+                        $e = Enchantment::getEnchantment((int)0);
+                        $leggings->addEnchantment(new EnchantmentInstance($e, (int)-0));
+                        $e = Enchantment::getEnchantment((int)0);
+                        $leggings->addEnchantment(new EnchantmentInstance($e, (int)-0));
+                        $boots = Item::get(313, 0, 1)->setCustomName("Test Boots");
+                        $e = Enchantment::getEnchantment((int)0);
+                        $boots->addEnchantment(new EnchantmentInstance($e, (int)-0));
+                        $e = Enchantment::getEnchantment((int)0);
+                        $boots->addEnchantment(new EnchantmentInstance($e, (int)-0));
+                        $player->getInventory()->addItem($helmet);
+                        $player->getInventory()->addItem($chestplate);
+                        $player->getInventory()->addItem($leggings);
+                        $player->getInventory()->addItem($boots);
+                        $player->getInventory()->removeItem($item);
+                    } else {
+                        $player->sendMessage(TextFormat::RED . "You do not have permission to use this kit!");
+                    }
                     break;
                 case 11:
                     //Put next kit items here
